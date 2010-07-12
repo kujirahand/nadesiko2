fd3ef333435e78744157b524fd5a3b86c8e21f6c
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2010-07-12 14:01:36 +0000

    Add TestCode

---
+++ trunk/TestNako/TestCNako.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Libnako.Parser;
+
+namespace TestNako
+{
+    [TestFixture]
+    class TestCNako
+    {
+        [Test(Description = "First Test")]
+        public void test1()
+        {
+            NakoDic dic = NakoDic.GetInstance();
+            if (dic.ContainsKey("hoge_")) {
+                Assert.Fail("hoge has not element!");
+            }
+        }
+    }
+}
