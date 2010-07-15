cfe0904b5bb96141f184eb23e612d4397d4e0cbc
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2010-07-15 18:27:40 +0000

    コードを整理した

---
+++ trunk/cnako2/Libnako/Parser/CalcType.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Libnako.Parser
+{
+    public class CalcType
+    {
+        // Define Token
+        //
+        public const int NOP = 0; // ???
+        //
+        public const int PLUS = 1; // +
+        public const int MINUS = 2; // -
+        public const int MUL = 3; // *
+        public const int DIV = 4; // /
+        public const int MOD = 5; // %
+        public const int POWER = 6; // ^
+        // comp
+        public const int EQ = 7; // ==
+        public const int NOT_EQ = 8; // <> !=
+        public const int GT = 9; // >
+        public const int GT_EQ = 10; // >=
+        public const int LT = 11; // <
+        public const int LT_EQ = 12; // <=
+        //
+        public const int AND = 13; // &&
+        public const int OR = 14; // ||
+        public const int XOR = 15; // ~~
+        //
+        public const int NEG = 16; // !
+
+        // Token Description
+        public static String[] TypeName = new String[] {
+" ???","+","-"," *"," /"," %","^"," ==","<> !=",
+" >",">="," <","<="," &&"," ||"," ~~"," !",
+        };
+        // Description Method
+        public static String GetTypeName(int no)
+        {
+            if (TypeName.Length > no) {
+                return TypeName[no];
+            }
+            else
+            {
+                return "UNKNOWN";
+            }
+        }
+    }
+/*
+        case CalcType.NOP: c.type = NakoILType.NOP; break;
+        case CalcType.PLUS: c.type = NakoILType.PLUS; break;
+        case CalcType.MINUS: c.type = NakoILType.MINUS; break;
+        case CalcType.MUL: c.type = NakoILType.MUL; break;
+        case CalcType.DIV: c.type = NakoILType.DIV; break;
+        case CalcType.MOD: c.type = NakoILType.MOD; break;
+        case CalcType.POWER: c.type = NakoILType.POWER; break;
+        case CalcType.EQ: c.type = NakoILType.EQ; break;
+        case CalcType.NOT_EQ: c.type = NakoILType.NOT_EQ; break;
+        case CalcType.GT: c.type = NakoILType.GT; break;
+        case CalcType.GT_EQ: c.type = NakoILType.GT_EQ; break;
+        case CalcType.LT: c.type = NakoILType.LT; break;
+        case CalcType.LT_EQ: c.type = NakoILType.LT_EQ; break;
+        case CalcType.AND: c.type = NakoILType.AND; break;
+        case CalcType.OR: c.type = NakoILType.OR; break;
+        case CalcType.XOR: c.type = NakoILType.XOR; break;
+        case CalcType.NEG: c.type = NakoILType.NEG; break;
+
+*/
+}
--- trunk/cnako2/Libnako/Parser/NakoILWriter.cs
+++ trunk/cnako2/Libnako/Parser/NakoILWriter.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace Libnako.Parser
 {
@@ -40,21 +42,33 @@ namespace Libnako.Parser
                 NakoNode n = node.Children[i];
                 Write_r(n);
             }
+            //
         }
 
         private void newCalc(NakoNodeCalc node)
         {
-            // TODO
+            NakoILCode c = new NakoILCode();
+
             switch (node.calc_type)
             {
-                case NakoNodeCalc.CalcType.Plus:
-                case NakoNodeCalc.CalcType.Minus:
-                case NakoNodeCalc.CalcType.Mul:
-                case NakoNodeCalc.CalcType.Div:
-                case NakoNodeCalc.CalcType.Mod:
-                case NakoNodeCalc.CalcType.Eq:
-                case NakoNodeCalc.CalcType.NotEq:
-                    break;
+                case CalcType.NOP:
+                    throw new Exception("NOP");
+                case CalcType.PLUS:     c.type = NakoILType.ADD; break;
+                case CalcType.MINUS:    c.type = NakoILType.SUB; break;
+                case CalcType.MUL: c.type = NakoILType.MUL; break;
+                case CalcType.DIV: c.type = NakoILType.DIV; break;
+                case CalcType.MOD: c.type = NakoILType.MOD; break;
+                case CalcType.POWER: c.type = NakoILType.POWER; break;
+                case CalcType.EQ: c.type = NakoILType.EQ; break;
+                case CalcType.NOT_EQ: c.type = NakoILType.NOT_EQ; break;
+                case CalcType.GT: c.type = NakoILType.GT; break;
+                case CalcType.GT_EQ: c.type = NakoILType.GT_EQ; break;
+                case CalcType.LT: c.type = NakoILType.LT; break;
+                case CalcType.LT_EQ: c.type = NakoILType.LT_EQ; break;
+                case CalcType.AND: c.type = NakoILType.AND; break;
+                case CalcType.OR: c.type = NakoILType.OR; break;
+                case CalcType.XOR: c.type = NakoILType.XOR; break;
+                case CalcType.NEG: c.type = NakoILType.NEG; break;
             }
         }
     }
