eefa79ef24ad447473812e5cdfc9831d2712705a
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2010-07-28 13:37:59 +0000

    Node の Namespace 不一致を修正。

--- cnako2/Libnako/Parser/NakoParserBase.cs
+++ cnako2/Libnako/Parser/NakoParserBase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Libnako.Parser.Node;
 
 namespace Libnako.Parser
 {
--- cnako2/Libnako/Parser/Node/NakoNodeCalc.cs
+++ cnako2/Libnako/Parser/Node/NakoNodeCalc.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Libnako.Parser
+namespace Libnako.Parser.Node
 {
     public class NakoNodeCalc : NakoNode
     {
