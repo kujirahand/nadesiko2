8b2b587d613e3d4130c8451051a28a102cae03d6
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2010-07-29 14:56:44 +0000

    もし構文のバグを修正。ネストをしている「もし」構文に対応。複数インデント解除のバグを修正。

--- cnako2/Program.cs
+++ cnako2/Program.cs
@@ -16,32 +16,40 @@ namespace cnako
             NakoNamespace ns = new NakoNamespace();
             NakoInterpreter runner = new NakoInterpreter();
 
-            /*
             String src =
                 "A=1\n" +
                 "B=2\n" +
-                "もし、A=Bならば\n" +
-                "  PRINT`真`\n" +
+                "もし、A=1ならば\n" +
+                "  もし、B=1ならば\n" +
+                "    PRINT `11`\n" +
+                "  違えば\n" +
+                "    PRINT `12`\n" +
                 "違えば\n" +
-                "  PRINT`偽`\n";
-             */
-
-            String src = "A=1;B=1;もし、A=Bならば、PRINT`OK`違えば,PRINT`NG`";
-
+                "  PRINT `*`"
+            ;
+            
+            /*
+            // TokenizerTest
             ns.source = src;
             ns.Tokenize();
             _w(ns.Tokens.toTypeString());
+            Console.WriteLine("End.");
+            Console.ReadLine();
+             */
 
-            ns.Publish();
+
+
+            ns.Publish(src);
 
             // DESCRIPT
             _w("token:" + ns.Tokens.toTypeString());
             _w("nodes:"+ns.TopNode.Children.toTypeString());
             _w("IL:\n"+ns.Codes.ToAddressString());
-
-            //
+            
             runner.Run(ns.Codes);
             _w(runner.PrintLog);
+
+            //
 
             //
             Console.WriteLine("End.");
