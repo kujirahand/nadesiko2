e2956cad81b05d317fb00ed172b7dbf3641d12e1
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2010-08-23 03:08:31 +0000

    関数実装のためのクラスを、Function パッケージにまとめた

---
+++ trunk/cnako2/Libnako/JCompiler/Function/NakoFuncArgs.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Libnako.JCompiler.Tokenizer;
+
+namespace Libnako.JCompiler.Function
+{
+    public class NakoFuncArgs : List<NakoFuncArg>
+    {
+        /// <summary>
+        /// 引数の定義文字列を読んで、関数の引数として登録する
+        /// </summary>
+        /// <param name="str"></param>
+        public void analizeArgStr(String str)
+        {
+            NakoTokenizer tokenizer = new NakoTokenizer(str);
+            tokenizer.splitWord();
+            NakoTokenList tokens = tokenizer.Tokens;
+
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                NakoToken tok = tokens[i];
+                NakoFuncArg arg = null;
+                if (tok.type == TokenType.WORD)
+                {
+                    int idx = indexOfName(tok.value);
+                    if (idx < 0)
+                    {
+                        arg = new NakoFuncArg();
+                        arg.name = tok.value;
+                        arg.AddJosi(tok.josi);
+                        this.Add(arg);
+                    }
+                    else
+                    {
+                        arg = this[idx];
+                        arg.AddJosi(tok.josi);
+                    }
+                }
+                if (tok.type == TokenType.OR || tok.type == TokenType.OR_OR)
+                {
+                    continue;
+                }
+
+            }
+        }
+
+        public int indexOfName(String name)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                NakoFuncArg arg = this[i];
+                if (arg.name == name)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
