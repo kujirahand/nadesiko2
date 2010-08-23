5eae621a8e707f04e7cbdd4856cb7de2a4fa76b9
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2010-08-23 03:08:31 +0000

    関数実装のためのクラスを、Function パッケージにまとめた

---
+++ cnako2/Libnako/JCompiler/Function/NakoFunc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Libnako.JCompiler.Function
+{
+    public class NakoFunc
+    {
+        public String name;
+        public NakoFuncArgs args;
+        public NakoFuncType funcType = NakoFuncType.UserCall; 
+
+        public NakoFunc()
+        {
+            Init();
+        }
+
+        public NakoFunc(String name, String argdef)
+        {
+            Init();
+            this.name = name;
+            this.args.analizeArgStr(argdef);
+        }
+
+        public virtual void Init()
+        {
+            args = new NakoFuncArgs();
+        }
+
+        public virtual void Execute() { }
+    }
+
+    public enum NakoFuncType
+    {
+        SysCall,
+        UserCall
+    }
+}
--- cnako2/Libnako/JCompiler/Function/NakoFuncArg.cs
+++ cnako2/Libnako/JCompiler/Function/NakoFuncArg.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Libnako.JCompiler
+namespace Libnako.JCompiler.Function
 {
     public class NakoFuncArg
     {
