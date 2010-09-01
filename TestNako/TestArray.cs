38ccd49f7a338125cb44f6cc3a908084f3e287ab
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2010-09-01 17:25:03 +0000

    一次元配列のテストを追加。

---
+++ TestNako/TestArray.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using Libnako.JCompiler;
+using Libnako.Interpreter;
+using Libnako.JCompiler.ILWriter;
+
+namespace TestNako
+{
+    [TestFixture]
+    class TestArray
+    {
+        NakoCompiler ns = new NakoCompiler();
+        NakoInterpreter runner = new NakoInterpreter();
+        NakoILCodeList codes = null;
+
+        [Test]
+        public void Test_array1()
+        {
+            codes = ns.Publish(
+                "A[3]=566\n"+
+                "A[3]を表示\n" + 
+                ""
+                );
+            runner.Run(codes);
+            Assert.AreEqual(runner.PrintLog, "566");
+        }
+
+        [Test]
+        public void Test_array2()
+        {
+            codes = ns.Publish(
+                "A[`a`]=566\n" +
+                "A[`a`]を表示\n" +
+                ""
+                );
+            runner.Run(codes);
+            Assert.AreEqual(runner.PrintLog, "566");
+        }
+
+        [Test]
+        public void Test_array3()
+        {
+            codes = ns.Publish(
+                "A[3]=566\n" +
+                "PRINT A[3]\n" +
+                ""
+                );
+            runner.Run(codes);
+            Assert.AreEqual(runner.PrintLog, "566");
+        }
+    }
+}
