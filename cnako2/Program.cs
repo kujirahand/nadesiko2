c004a41198dc0e17fc3a66655adce3276bb147fc
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2010-09-28 15:05:48 +0000

    コマンドラインから cnako2 を使えるようにした。

--- cnako2/Program.cs
+++ cnako2/Program.cs
@@ -11,38 +11,151 @@ namespace cnako2
 {
     class Program
     {
+        static bool DescriptMode = false;
+        static bool DebugMode = false;
+        static bool WaitMode = false;
+
+        static void ShowHelp()
+        {
+            _w("# [CNAKO2]");
+            _w("# USAGE:");
+            _w("# >cnako2 [-desc][-debug][-wait][-e (code)]|[(sourcefile)]");
+            _w("# Example:");
+            _w("# >cnako2 (sourcefile)");
+            _w("# >cnako2 -e (one liner code)");
+        }
+        
         static void Main(string[] args)
         {
+            
             // --------------------------------------------------
-            // Compile
+            // Read Source Code
+            if (args.Length == 0)
+            {
+                ShowHelp();
+                return;
+            }
+            // --------------------------------------------------
+            NakoConsoleMode mode = NakoConsoleMode.RunFile;
+            string source = null;
+
+            int i = 0;
+            while (i < args.Length)
+            {
+                string arg = args[i];
+                // eval
+                if (arg == "-e" || arg == "-eval")
+                {
+                    mode = NakoConsoleMode.OneLiner;
+                    i++;
+                    continue;
+                }
+                if (arg == "-desc" || arg == "-descript")
+                {
+                    DescriptMode = true;
+                    continue;
+                }
+                if (arg == "-debug")
+                {
+                    DebugMode = true;
+                    continue;
+                }
+                if (arg == "wait")
+                {
+                    WaitMode = true;
+                    continue;
+                }
+                // other
+                if (source == null)
+                {
+                    source = args[i];
+                    i++;
+                    continue;
+                }
+                i++;
+            }
+
+            switch (mode)
+            {
+                case NakoConsoleMode.OneLiner:
+                    OneLinerMode(source);
+                    break;
+                case NakoConsoleMode.RunFile:
+                    RunFileMode(source);
+                    break;
+            }
+            if (WaitMode)
+            {
+                Console.ReadLine();
+            }
+
+        }
+
+        static void OneLinerMode(string code)
+        {
             NakoCompiler compiler = new NakoCompiler();
-            compiler.DirectSource =
-                "OS\n" +
-                "それを表示\n" +
-                "";
-            cout = "----------";
-            cout = "* TOKENS:";
-            cout = compiler.Tokens.toTypeString();
-            cout = "----------";
-            cout = "* NODES:";
-            cout = compiler.TopNode.Children.toTypeString();
-            cout = "----------";
-            cout = "* CODES:";
-            cout = compiler.Codes.ToAddressString();
+            compiler.DirectSource = code;
+            if (DescriptMode)
+            {
+                cout = "----------";
+                cout = "* TOKENS:";
+                cout = compiler.Tokens.toTypeString();
+                cout = "----------";
+                cout = "* NODES:";
+                cout = compiler.TopNode.Children.toTypeString();
+                cout = "----------";
+                cout = "* CODES:";
+                cout = compiler.Codes.ToAddressString();
+                cout = "----------";
+                cout = "* RUN";
+                NakoInterpreter runner = new NakoInterpreter(compiler.Codes);
+                runner.debugMode = DebugMode;
+                runner.Run();
+                Console.WriteLine("LOG=" + runner.PrintLog);
+                cout = "----------";
+                cout = "ok.";
+            }
+            else
+            {
+                NakoInterpreter runner = new NakoInterpreter(compiler.Codes);
+                runner.debugMode = DebugMode;
+                runner.Run();
+                Console.WriteLine(runner.PrintLog);
+            }
+        }
 
-            // --------------------------------------------------
-            // Run
-            cout = "----------";
-            cout = "* RUN";
-            NakoInterpreter runner = new NakoInterpreter(compiler.Codes);
-            runner.debugMode = true;
-            runner.Run();
-            Console.WriteLine("LOG="+runner.PrintLog);
-            cout = "----------";
-
-            // Wait
-            cout = "ok.";
-            Console.ReadLine();
+        static void RunFileMode(string sourcefile)
+        {
+            NakoLoader loader = NakoLoader.Instance;
+            loader.LoadFromFile(sourcefile);
+            NakoCompiler compiler = loader.cur;
+            if (DescriptMode)
+            {
+                cout = "----------";
+                cout = "* TOKENS:";
+                cout = compiler.Tokens.toTypeString();
+                cout = "----------";
+                cout = "* NODES:";
+                cout = compiler.TopNode.Children.toTypeString();
+                cout = "----------";
+                cout = "* CODES:";
+                cout = compiler.Codes.ToAddressString();
+                cout = "----------";
+                cout = "* RUN";
+                NakoInterpreter runner = new NakoInterpreter(compiler.Codes);
+                runner.debugMode = DebugMode;
+                runner.Run();
+                Console.WriteLine("LOG=" + runner.PrintLog);
+                cout = "----------";
+                cout = "ok.";
+            }
+            else
+            {
+                NakoInterpreter runner = new NakoInterpreter(compiler.Codes);
+                runner.debugMode = DebugMode;
+                runner.Run();
+                Console.WriteLine(runner.PrintLog);
+            }
         }
 
         static void _w(string s)
@@ -54,5 +167,11 @@ namespace cnako2
         {
             set { Console.WriteLine(value); }
         }
+    }
+
+    enum NakoConsoleMode
+    {
+        OneLiner,
+        RunFile
     }
 }
