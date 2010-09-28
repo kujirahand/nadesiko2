578ada9df0ef9a41e8be1277b98418de2267f573
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2010-09-28 14:08:43 +0000

    .Net Framework3.0でもコンパイルできるように修正。

--- trunk/Libnako/Interpreter/NakoInterpreter.cs
+++ trunk/Libnako/Interpreter/NakoInterpreter.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
 using System.Text;
 using Libnako.JCompiler.ILWriter;
 using Libnako.JCompiler;
--- trunk/Libnako/NakoAPI/NakoAPIFuncBank.cs
+++ trunk/Libnako/NakoAPI/NakoAPIFuncBank.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using Libnako.Interpreter;
 using Libnako.JCompiler.Function;
