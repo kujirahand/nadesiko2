6b5e543bcdf3a9568f0b3224eb64a753dc54ac1e
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2010-09-28 15:05:48 +0000

    コマンドラインから cnako2 を使えるようにした。

---
+++ trunk/DemoCNako2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Libnako.JCompiler;
+using Libnako.Interpreter;
+using Libnako.JCompiler.ILWriter;
+using Libnako.JCompiler.Tokenizer;
+
+namespace DemoCNako2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // --------------------------------------------------
+            // Compile
+            NakoCompiler compiler = new NakoCompiler();
+            compiler.DirectSource =
+                "OS\n" +
+                "それを表示\n" +
+                "";
+            cout = "----------";
+            cout = "* TOKENS:";
+            cout = compiler.Tokens.toTypeString();
+            cout = "----------";
+            cout = "* NODES:";
+            cout = compiler.TopNode.Children.toTypeString();
+            cout = "----------";
+            cout = "* CODES:";
+            cout = compiler.Codes.ToAddressString();
+
+            // --------------------------------------------------
+            // Run
+            cout = "----------";
+            cout = "* RUN";
+            NakoInterpreter runner = new NakoInterpreter(compiler.Codes);
+            runner.debugMode = true;
+            runner.Run();
+            Console.WriteLine("LOG=" + runner.PrintLog);
+            cout = "----------";
+
+            // Wait
+            cout = "ok.";
+            Console.ReadLine();
+        }
+
+        static void _w(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        static string cout
+        {
+            set { Console.WriteLine(value); }
+        }
+    }
+}
