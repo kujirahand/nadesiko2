5a3bf92051a3d6a4ed89a37da7e5b3ef614e70ba
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2010-09-29 06:23:15 +0000

    プラグインに「保存」「開く」命令を追加。 プロジェクトの設定がおかしくなっていたので修正。

--- trunk/NakoPluginFile/NakoPluginFile.cs
+++ trunk/NakoPluginFile/NakoPluginFile.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 
 using NakoPlugin;
+using Libnako.NakoAPI.WrapLib;
 
-
-namespace NakoPluginCtrl
+namespace NakoPluginFile
 {
-    public class NakoPluginCtrl : INakoPlugin
+    public class NakoPluginFile : INakoPlugin
     {
         public string Name
         {
@@ -26,10 +26,29 @@ namespace NakoPluginCtrl
 
         public void DefineFunction(INakoPluginBank bank)
         {
-            //bank.AddFunc("コピー", "Sを|Sの", NakoVarType.Void, _copyToClipboard, "文字列Sをクリップボードにコピーする", "こぴー");
-            //bank.AddFunc("クリップボード", "", NakoVarType.Void, _getFromClipboard, "クリップボードの文字列を取得する", "くりっぷぼーど");
+            bank.AddFunc("開く", "FILEを|FILEから", NakoVarType.String, _openFile, "ファイル名FILEのテキストを全部読み込んで返す。この時、自動的に文字コードを判定して読み込む。", "ひらく");
+            bank.AddFunc("保存", "SをFILEに|FILEへ", NakoVarType.Void, _saveFile, "文字列Sをファイル名FILEへ保存する。(文字コードUTF-8で保存される)", "ほぞん");
         }
 
         // Define Method
+        public Object _openFile(INakoFuncCallInfo info)
+        {
+            String fileName = info.StackPopAsString();
+            // Exists?
+            if (!System.IO.File.Exists(fileName))
+            {
+                throw new NakoPluginRuntimeException("ファイル『" + fileName + "』は存在しません。");
+            }
+            // Load
+            String src = StrUnit.LoadFromFileAutoEnc(fileName);
+            return src;
+        }
+        public Object _saveFile(INakoFuncCallInfo info)
+        {
+            String s = info.StackPopAsString();
+            String fileName = info.StackPopAsString();
+            System.IO.File.WriteAllText(fileName, s, Encoding.UTF8);
+            return null;
+        }
     }
 }
