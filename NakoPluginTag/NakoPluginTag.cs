5bf43bcc8a7a4bfc335fffa1ba028cf99ce360be
shigepon0@gmail.com <shigepon0@gmail.com@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2011-04-05 14:44:18 +0000

    XML, HTML処理関数を一部追加

---
+++ NakoPluginTag/NakoPluginTag.cs
@@ -0,0 +1,206 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: shigepon
+ * Date: 2011/03/31
+ * Time: 9:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using Libnako.JPNCompiler;
+using NakoPlugin;
+using System.IO;
+
+using HtmlAgilityPack;
+
+namespace NakoPluginTag
+{
+    /// <summary>
+    /// Description of NakoPluginTag.
+    /// </summary>
+    public class NakoPluginTag : INakoPlugin
+    {
+        string _description = "XML,HTMLタグ処理を行うプラグイン";
+        double _version = 1.0;
+        //--- プラグイン共通の部分 ---
+        public double TargetNakoVersion { get { return 2.0; } }
+        public bool Used { get; set; }
+        public string Name { get { return this.GetType().FullName; } }
+        public double PluginVersion { get { return _version; } }
+        public string Description { get { return _description; } }
+        //--- 関数の定義 ---
+        public void DefineFunction(INakoPluginBank bank)
+        {
+            //+日付時間処理
+            //-日付時間
+            bank.AddFunc("タグ切り出し", "SからAの|SでAを", NakoVarType.Array, _extract, "SからAのタグを切り取る。", "たぐきりだし");
+            bank.AddFunc("階層タグ切り出し", "SからAの|SでAを", NakoVarType.Array, _hirextract, "Sから特定階層下のタグAを切り取る。たとえば『head/title』『item/link』など。", "かいそうたぐきりだし");
+            bank.AddFunc("タグ属性取得", "SのAからBを|Sで", NakoVarType.Array, _attr, "SからタグAの属性Bを取り出す。", "たぐぞくせいしゅとく");
+            bank.AddFunc("タグで削除", "Sから|Sの", NakoVarType.String, _remove, "Sのタグを削除。", "たぐさくじょ");
+            bank.AddFunc("タグ属性一覧取得", "SからAの|Sで", NakoVarType.Array, _attrlist, "SからタグAにある属性をハッシュ形式で取得する。", "たぐぞくせいいちらんしゅとく");
+            bank.AddFunc("HTMLリンク抽出", "Sから|Sの", NakoVarType.Array, _link, "SからHTMLのリンク（A,IMGタグ）を抽出して返す。", "えいちてぃーえむえるりんくちゅうしゅつ");
+        }
+        
+        // プラグインの初期化処理
+        public void PluginInit(INakoInterpreter runner)
+        {
+        }
+        // プラグインの終了処理
+        public void PluginFin(INakoInterpreter runner)
+        {
+        }
+        public Object _extract(INakoFuncCallInfo info)
+        {
+        	String s = info.StackPopAsString();
+        	String a = info.StackPopAsString();
+        	HtmlDocument doc = new HtmlDocument();
+        	doc.LoadHtml(s);
+        	HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes(String.Format(@"//{0}",a));
+        	NakoVarArray res = new NakoVarArray();
+        	if(nodes!=null){
+            	for(int i=0;i<nodes.Count;i++){
+            	    HtmlNode node = nodes[i];
+            	    res.SetValue(i,node.InnerHtml);
+            	}
+        	}
+            return res;
+        }
+         public Object _hirextract(INakoFuncCallInfo info)
+        {
+        	String s = info.StackPopAsString();
+        	String a = info.StackPopAsString();
+        	HtmlDocument doc = new HtmlDocument();
+        	doc.LoadHtml(s);
+        	HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes(String.Format(@"//{0}",a));
+        	NakoVarArray res = new NakoVarArray();
+        	if(nodes!=null){
+            	for(int i=0;i<nodes.Count;i++){
+            	    HtmlNode node = nodes[i];
+            	    res.SetValue(i,node.InnerHtml);
+            	}
+        	}
+            return res;
+        }
+       public Object _attr(INakoFuncCallInfo info)
+        {
+        	String s = info.StackPopAsString();
+        	String a = info.StackPopAsString();
+        	String b = info.StackPopAsString();
+        	HtmlDocument doc = new HtmlDocument();
+        	doc.LoadHtml(s);
+        	HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes(String.Format(@"//{0}",a));
+        	NakoVarArray res = new NakoVarArray();
+        	if(nodes!=null){
+            	for(int i=0;i<nodes.Count;i++){
+            	    HtmlNode node = nodes[i];
+            	    res.SetValue(i,node.GetAttributeValue(b,""));
+            	}
+        	}
+            return res;
+        }
+       public Object _attrlist(INakoFuncCallInfo info)
+        {
+        	String s = info.StackPopAsString();
+        	String a = info.StackPopAsString();
+        	HtmlDocument doc = new HtmlDocument();
+        	doc.LoadHtml(s);
+        	HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes(String.Format(@"//{0}",a));
+        	NakoVarArray res = new NakoVarArray();
+        	if(nodes!=null){
+            	foreach(HtmlNode node in nodes){
+        	        foreach(HtmlAttribute attr in node.Attributes){
+            	       res.SetValueFromKey(attr.Name,attr.Value);
+        	        }
+            	}
+        	}
+            return res;
+        }
+        public Object _link(INakoFuncCallInfo info)
+        {
+        	String s = info.StackPopAsString();
+        	HtmlDocument doc = new HtmlDocument();
+        	doc.LoadHtml(s);
+        	HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes(@"//a");
+        	NakoVarArray res = new NakoVarArray();
+        	if(nodes!=null){
+            	for(int i=0;i<nodes.Count;i++){
+            	    HtmlNode node = nodes[i];
+            	    res.SetValue(i,node.GetAttributeValue("href",""));
+            	}
+        	}
+            return res;
+        }
+        public Object _remove(INakoFuncCallInfo info)
+        {
+        	String s = info.StackPopAsString();
+        	HtmlDocument doc = new HtmlDocument();
+         	doc.LoadHtml(s);
+            StringWriter sw = new StringWriter();
+            ConvertTo(doc.DocumentNode, sw);
+            sw.Flush();
+            return sw.ToString();
+        }
+        
+        private void ConvertTo(HtmlNode node, TextWriter outText)
+        {
+            string html;
+            switch (node.NodeType)
+            {
+                case HtmlNodeType.Comment:
+                    // don't output comments
+                    break;
+
+                case HtmlNodeType.Document:
+                    ConvertContentTo(node, outText);
+                    break;
+
+                case HtmlNodeType.Text:
+                    // script and style must not be output
+                    string parentName = node.ParentNode.Name;
+                    if ((parentName == "script") || (parentName == "style"))
+                        break;
+
+                    // get text
+                    html = ((HtmlTextNode) node).Text;
+
+                    // is it in fact a special closing node output as text?
+                    if (HtmlNode.IsOverlappedClosingElement(html))
+                        break;
+
+                    // check the text is meaningful and not a bunch of whitespaces
+                    if (html.Trim().Length > 0)
+                    {
+                        outText.Write(HtmlEntity.DeEntitize(html));
+                    }
+                    break;
+
+                case HtmlNodeType.Element:
+                    switch (node.Name)
+                    {
+                        case "p":
+                            // treat paragraphs as crlf
+                            outText.Write("\r\n");
+                            break;
+                    }
+
+                    if (node.HasChildNodes)
+                    {
+                        ConvertContentTo(node, outText);
+                    }
+                    break;
+            }
+        }
+        
+         private void ConvertContentTo(HtmlNode node, TextWriter outText)
+        {
+            foreach (HtmlNode subnode in node.ChildNodes)
+            {
+                ConvertTo(subnode, outText);
+            }
+        }
+       
+    }
+}
