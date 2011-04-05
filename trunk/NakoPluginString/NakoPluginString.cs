b399dd965a4d5dc0ea1f2d15d49f48fc23d494d1
shigepon0@gmail.com <shigepon0@gmail.com@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2011-04-05 14:42:09 +0000

    文字列関数を追加

--- trunk/NakoPluginString/NakoPluginString.cs
+++ trunk/NakoPluginString/NakoPluginString.cs
@@ -8,8 +8,9 @@
  */
 using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading;
-
+using Libnako.JPNCompiler;
 using NakoPlugin;
 
 namespace NakoPluginString
@@ -38,6 +39,17 @@ namespace NakoPluginString
             bank.AddFunc("単置換", "SのSEARCHをREPLACEに", NakoVarType.String, _replace_a, "文字列を一つだけ置換して返す", "たんちかん");
             bank.AddFunc("文字左部分", "SのNUM", NakoVarType.String, _left, "文字列の指定文字数左部分を抜き出す", "もじひだりぶぶん");
             bank.AddFunc("トリム", "Sを", NakoVarType.String, _trim, "文字列をトリムする", "とりむ");
+
+            bank.AddFunc("文字右部分", "SのNUM", NakoVarType.String, _right, "文字列の指定文字数右部分を抜き出す", "もじみぎぶぶん");
+            bank.AddFunc("切り取る", "{参照渡し}SからAまで|SのAまでを|SでAを", NakoVarType.String, _cut, "文字列Sから区切り文字Aまでを切り取って返す。Sに変数を指定した場合はSの内容が切り取られる。", "きりとる");
+            bank.AddFunc("文字抜き出す", "SのAからCNT", NakoVarType.String, _extract, "文字列SでAからCNT文字分を抜き出して返す", "もじぬきだす");
+            bank.AddFunc("全角か判定", "Sが|Sの|Sを", NakoVarType.Int, _Em, "文字列Sの一文字目が全角かどうか判定して返す。", "ぜんかくかはんてい");
+            bank.AddFunc("文字削除", "{参照渡し}SのAからB|Sで", NakoVarType.Void, _remove, "文字列SのA文字目からB文字だけ削除する。Sに変数を指定した場合はSの内容も変更する", "もじさくじょ");
+            bank.AddFunc("文字挿入", "SのCNTにAを", NakoVarType.String, _insert, "文字列SのCNT文字目に文字列Aを挿入して返す。", "もじそうにゅう");
+            bank.AddFunc("文字列分解", "Sを|Sの|Sで", NakoVarType.Array, _degrade, "文字列Sを1文字ずつ配列変数に分解する。", "もじれつぶんかい");
+            bank.AddFunc("区切る", "SをAで", NakoVarType.Array, _explode, "文字列Sを区切り文字Aで区切って配列として返す。", "くぎる");
+            bank.AddFunc("数字か判定", "Sが|Sの|Sを", NakoVarType.Int, _num, "文字列Sの一文字目が数字か判定して返す", "すうじかはんてい");
+            bank.AddFunc("追加", "AにBを|Aへ", NakoVarType.String, _append, "変数AにBの内容を追加する", "ついか");
         }
         
         // プラグインの初期化処理
@@ -89,6 +101,102 @@ namespace NakoPluginString
         	String s = info.StackPopAsString();
         	return s.Trim();
         }
-       
+        public Object _right(INakoFuncCallInfo info){
+        	String s = info.StackPopAsString();
+        	int len = (int)info.StackPopAsInt();
+        	return s.Substring(s.Length-len);
+        }
+        public Object _cut(INakoFuncCallInfo info){
+            Object sr = info.StackPop();
+            String a = info.StackPopAsString();
+            string[] delim = {a};
+            if(!(sr is NakoVariable)){
+                throw new ApplicationException("『切り取る』に変数が設定されていません");
+            }
+            Object s = ((NakoVariable)sr).Body;
+            Object ret;
+            if(s is String){
+                string[] split_s = ((String)s).Split(delim,2,StringSplitOptions.None);
+                if(split_s.Length==2){
+                   ret = split_s[1];
+                   ((NakoVariable)sr).Body = ret;
+                    return split_s[0];
+                }
+            }
+            return null;
+        }
+        public Object _extract(INakoFuncCallInfo info){
+            String s = info.StackPopAsString();
+            int a = NadesikoPositionToCSPosition((int)info.StackPopAsInt());
+            int cnt = (int)info.StackPopAsInt();
+            return s.Substring(a,cnt);
+        }
+        public Object _Em(INakoFuncCallInfo info){
+            String s = info.StackPopAsString();
+            char c1 = s[0];
+            int num = Encoding.GetEncoding("Shift_JIS").GetByteCount(c1.ToString());
+            if(num==2){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+        public Object _remove(INakoFuncCallInfo info){
+            Object sr = info.StackPop();
+            int a = NadesikoPositionToCSPosition((int)info.StackPopAsInt());
+            int b = (int)info.StackPopAsInt();
+            Object s = ((NakoVariable)sr).Body;
+            Object ret;
+            if(s is String){
+                ret = ((String)s).Remove(a,b);
+            }else{
+                ret = null;
+            }
+            ((NakoVariable)sr).Body = ret;
+            return null;
+        }
+        public Object _insert(INakoFuncCallInfo info){
+            StringBuilder s = new StringBuilder(info.StackPopAsString());
+            int cnt = NadesikoPositionToCSPosition((int)info.StackPopAsInt());
+            String a = info.StackPopAsString();
+            return s.Insert(cnt,a).ToString();
+        }
+        public Object _degrade(INakoFuncCallInfo info){
+            String s = info.StackPopAsString();
+            char[] splitted = s.ToCharArray();
+             NakoVarArray arr = new NakoVarArray();
+            for(int i=0;i<splitted.Length;i++){
+                arr.SetValue(i,splitted[i]);
+            }
+            return arr;
+       }
+        public Object _explode(INakoFuncCallInfo info){
+            String s = info.StackPopAsString();
+            String a = info.StackPopAsString();
+            string[] splitted = s.Split(new string[]{a},StringSplitOptions.None);
+            NakoVarArray arr = new NakoVarArray();
+            for(int i=0;i<splitted.Length;i++){
+                arr.SetValue(i,splitted[i]);
+            }
+            return arr;
+        }
+        public Object _num(INakoFuncCallInfo info){
+            String s = info.StackPopAsString();
+            char c1 = s[0];
+            int num;
+            if(int.TryParse(c1.ToString(), out num)){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+        public Object _append(INakoFuncCallInfo info){
+            StringBuilder s = new StringBuilder(info.StackPopAsString());
+            String a = info.StackPopAsString();
+            return s.Append(a).ToString();
+        }
+        private int NadesikoPositionToCSPosition(int nadesiko_pos){
+            return nadesiko_pos - 1;
+        }
     }
 }
