21d09355abfc3cb93559b5c85626263c8134b8fd
shigepon0@gmail.com <shigepon0@gmail.com@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2011-04-27 15:19:22 +0000

    HTTP, 正規表現, ADOのテスト追加

---
+++ TestNako/TestNakoPluginADO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Libnako.JPNCompiler;
+using Libnako.Interpreter;
+using Libnako.NakoAPI;
+
+using Libnako.JPNCompiler.ILWriter;
+
+using NakoPluginADO;
+
+namespace TestNako
+{
+    [TestFixture]
+    public class TestNakoPluginADO
+    {
+        NakoCompiler com;
+        NakoInterpreter runner = new NakoInterpreter();
+
+        public TestNakoPluginADO()
+        {
+            NakoCompilerLoaderInfo info = new NakoCompilerLoaderInfo();
+            info.PreloadModules = new NakoPlugin.INakoPlugin[] {
+                new NakoBaseSystem(),
+                new NakoPluginADO.NakoPluginADO()
+            };
+            com = new NakoCompiler(info);
+        }
+
+        [Test]
+        public void TestOpenClose()
+        {
+            com.DirectSource = 
+                "HANDLE=「Driver=PostgreSQL Unicode;Server=localhost;Database=test;UID=postgres;PWD=bommer;Port=5432;」でADO開く\n" +
+                "HANDLEをDB閉じる";
+            runner.Run(com.Codes);
+//            Assert.AreEqual("なでしこ", runner.PrintLog );
+        }
+
+        [Test]
+        public void TestFind()
+        {
+            com.DirectSource = 
+                "HANDLE=「Driver=PostgreSQL Unicode;Server=localhost;Database=test;UID=postgres;PWD=bommer;Port=5432;」でADO開く\n" +
+                "HANDLEに「select * from test_table;」をSQL実行\n"+
+                "HANDLEのDBデータ有りの間\n"+
+                "\tF=HANDLEへ「hoge」のDBフィールド取得\n"+
+                "\tFを表示\n"+
+                "\tHANDLEのDB次移動\n"+
+                "HANDLEをDB閉じる";
+            runner.Run(com.Codes);
+            Assert.AreEqual("なでしこ", runner.PrintLog );
+        }
+
+     }
+}
---
+++ TestNako/TestNakoPluginHttp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Libnako.JPNCompiler;
+using Libnako.Interpreter;
+using Libnako.NakoAPI;
+
+using Libnako.JPNCompiler.ILWriter;
+
+using NakoPluginHttp;
+
+namespace TestNako
+{
+    [TestFixture]
+    public class TestNakoPluginHttp
+    {
+        NakoCompiler com;
+        NakoInterpreter runner = new NakoInterpreter();
+
+        public TestNakoPluginHttp()
+        {
+            NakoCompilerLoaderInfo info = new NakoCompilerLoaderInfo();
+            info.PreloadModules = new NakoPlugin.INakoPlugin[] {
+                new NakoBaseSystem(),
+                new NakoPluginHttp.NakoPluginHttp()
+            };
+            com = new NakoCompiler(info);
+        }
+
+        [Test]
+        public void TestUrlDecode()
+        {
+            com.DirectSource = 
+                "「%e3%81%aa%e3%81%a7%e3%81%97%e3%81%93」をURLデコードして表示。";
+            runner.Run(com.Codes);
+            Assert.AreEqual("なでしこ", runner.PrintLog );
+        }
+
+     }
+}
