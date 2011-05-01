25f88f9b720ddad96f7e4e68318baa8d2efb5dcc
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2011-05-01 15:39:25 +0000

    改めてプラグインのテストを追加。

---
+++ trunk/NakoPluginTest/TestNakoPluginDateTime.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Libnako.JPNCompiler;
+using Libnako.Interpreter;
+using Libnako.NakoAPI;
+
+using Libnako.JPNCompiler.ILWriter;
+
+using NakoPluginDateTime;
+
+namespace NakoPluginTest
+{
+    [TestFixture]
+    public class TestNakoPluginDateTime
+    {
+        NakoCompiler com;
+        NakoInterpreter runner = new NakoInterpreter();
+
+        public TestNakoPluginDateTime()
+        {
+            NakoCompilerLoaderInfo info = new NakoCompilerLoaderInfo();
+            info.PreloadModules = new NakoPlugin.INakoPlugin[] {
+                new NakoBaseSystem(),
+                new NakoPluginDateTime.NakoPluginDateTime()
+            };
+            com = new NakoCompiler(info);
+        }
+
+        [Test]
+        public void TestToday()
+        {
+            com.DirectSource = 
+                "今日を表示。";
+            runner.Run(com.Codes);
+            Assert.AreEqual(runner.PrintLog, DateTime.Today.ToString("yyyy-MM-dd"));
+        }
+
+        [Test]
+        public void TestNow()
+        {
+            com.DirectSource = 
+                "今を表示。";
+            runner.Run(com.Codes);
+            Assert.AreEqual(runner.PrintLog, DateTime.Now.ToString("HH:mm:ss"));
+        }
+        
+        [Test]
+        public void TestDiffHours()
+        {
+            com.DirectSource = 
+                "「2001-11-11 01:00:00」と「2001-11-11 02:00:00」の時間差を表示。";
+            runner.Run(com.Codes);
+            DateTime d1 = DateTime.Parse("2001-11-11 01:00:00");
+            DateTime d2 = DateTime.Parse("2001-11-11 02:00:00");
+            TimeSpan diff = d1.Subtract(d2);
+            Assert.AreEqual(runner.PrintLog, diff.TotalHours.ToString());
+        }
+    }
+}
---
+++ trunk/NakoPluginTest/TestNakoPluginSample.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Libnako.JPNCompiler;
+using Libnako.Interpreter;
+using Libnako.NakoAPI;
+
+using NakoPluginSample;
+using Libnako.JPNCompiler.ILWriter;
+
+namespace NakoPluginTest
+{
+    [TestFixture]
+    public class TestNakoPluginSample
+    {
+        NakoCompiler com;
+        NakoInterpreter runner = new NakoInterpreter();
+
+        public TestNakoPluginSample()
+        {
+            NakoCompilerLoaderInfo info = new NakoCompilerLoaderInfo();
+            info.PreloadModules = new NakoPlugin.INakoPlugin[] {
+                new NakoBaseSystem(),
+                new NakoPluginSample.NakoPluginSample()
+            };
+            com = new NakoCompiler(info);
+        }
+
+        [Test]
+        public void TestSetVariable()
+        {
+            com.DirectSource = 
+                "HOGE=「ふが」\n" +
+                "「はげ」へ変数HOGE書換\n" +
+                "HOGEを表示";
+            runner.Run(com.Codes);
+            Assert.AreEqual("はげ", runner.PrintLog );
+        }
+
+        [Test]
+        public void TestAddEx()
+        {
+            com.DirectSource = 
+                "A=「ほげ」\n"+
+                "Aに「ほげ」を接続!\n" +
+                "Aを表示";
+            runner.Run(com.Codes);
+            Assert.AreEqual("ほげほげ", runner.PrintLog );
+        }
+
+     }
+}
