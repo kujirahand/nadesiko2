8b73e3aca5ca3ad2f4ce7bd51a30e3a040aeb5b1
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2011-05-01 16:02:45 +0000

    オフィスプラグイン開発ソリューションを作成。

---
+++ trunk/NakoPluginOfficeTest/NakoPluginOfficeExcelTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using Libnako.JPNCompiler;
+using Libnako.Interpreter;
+using Libnako.NakoAPI;
+
+using NakoPluginOfficeExcel;
+
+namespace NakoPluginOfficeTest
+{
+    [TestFixture]
+    public class NakoPluginOfficeExcelTest
+    {
+        NakoCompiler com;
+        NakoInterpreter runner;
+
+        public NakoPluginOfficeExcelTest()
+        {
+            NakoCompilerLoaderInfo info = new NakoCompilerLoaderInfo();
+            info.PreloadModules = new NakoPlugin.INakoPlugin[] {
+                new NakoBaseSystem(),
+                new NakoPluginOfficeExcel.NakoPluginOfficeExcel()
+            };
+            com = new NakoCompiler(info);
+            runner = new NakoInterpreter();
+        }
+
+        [Test]
+        public void ExcelStart()
+        {
+            com.DirectSource = "1でエクセル起動。エクセル終了。";
+            runner.Run(com.Codes);
+            Assert.AreEqual(1, 1); // dummy
+        }
+    }
+}
