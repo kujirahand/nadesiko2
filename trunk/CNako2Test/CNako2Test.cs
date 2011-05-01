761d28f7bf3a5c3cfde7e8dd8a18c33c92a01ccf
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2011-05-01 14:18:51 +0000

    なんと、コメントをサポートしてなkったので、急いで実装。

--- trunk/CNako2Test/CNako2Test.cs
+++ trunk/CNako2Test/CNako2Test.cs
@@ -17,7 +17,7 @@ namespace CNako2Test
         public void Test_setOptions()
         {
             CNako2Executor e = new CNako2Executor();
-            string line = "「あ」と表示";
+            string line = "「あ」と継続表示";
             string[] args = {"-e", line};
             e.UseLog = true;
             e.setOptions(args);
@@ -29,7 +29,7 @@ namespace CNako2Test
         public void Test_Run_Oneliner()
         {
             CNako2Executor e = new CNako2Executor();
-            string line = "「あ」と表示";
+            string line = "「あ」と継続表示";
             string[] args = { "-e", line };
             e.UseLog = true;
             e.setOptions(args);
@@ -40,7 +40,7 @@ namespace CNako2Test
         public void Test_Run_Oneliner_Calc()
         {
             CNako2Executor e = new CNako2Executor();
-            string line = "3+5*2と表示";
+            string line = "3+5*2と継続表示";
             string[] args = { "-e", line };
             e.UseLog = true;
             e.setOptions(args);
@@ -57,6 +57,34 @@ namespace CNako2Test
             e.setOptions(args);
             e.Run();
             Assert.AreEqual("12", e.PrintLog);
+        }
+        [Test]
+        public void Test_Run_Oneliner_Comment()
+        {
+            CNako2Executor e = new CNako2Executor();
+            string line = "`abc`を表示。\n" +
+                "# comment\n" +
+                "`cde`を表示。\n" +
+                "";
+            string[] args = { "-e", line };
+            e.UseLog = true;
+            e.setOptions(args);
+            e.Run();
+            Assert.AreEqual("abc\r\ncde\r\n", e.PrintLog);
+        }
+        [Test]
+        public void Test_Run_Oneliner_Comment2()
+        {
+            CNako2Executor e = new CNako2Executor();
+            string line = "`abc`を表示。\n" +
+                "/* comment test\n --- abc def \nghi 333 222 */\n" +
+                "`cde`を表示。\n" +
+                "";
+            string[] args = { "-e", line };
+            e.UseLog = true;
+            e.setOptions(args);
+            e.Run();
+            Assert.AreEqual("abc\r\ncde\r\n", e.PrintLog);
         }
     }
 }
