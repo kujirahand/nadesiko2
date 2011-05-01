70b7a20508182f97ccc1ff9fe49aac58ae69ef71
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2011-05-01 17:26:39 +0000

    実行ファイルでプラグインをチェックしたい時のためのソリューションを追加。

---
+++ Nako2_CNakoDemo/CNako2Demo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Libnako.JPNCompiler;
+using Libnako.Interpreter;
+
+namespace DemoCNako2
+{
+    class Program
+    {
+        static NakoCompiler nc2 = new NakoCompiler();
+        static NakoInterpreter ni2 = new NakoInterpreter();
+        
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // --------------------------------------------------
+            // Compile
+            NakoCompiler compiler = new NakoCompiler();
+            compiler.DirectSource =
+                "デスクトップ。\nそれのファイル列挙。\nそれを「/」で配列結合。\nそれを表示。" +
+                "";
+            cout = "----------";
+            cout = "* TOKENS:";
+            cout = compiler.Tokens.toTypeString();
+            cout = "----------";
+            cout = "* NODES:";
+            cout = compiler.TopNode.Children.toTypeString();
+            cout = "----------";
+            cout = "* CODES:";
+            cout = compiler.Codes.ToAddressString();
+
+            // --------------------------------------------------
+            // Run
+            cout = "----------";
+            cout = "* RUN";
+            NakoInterpreter runner = new NakoInterpreter(compiler.Codes);
+            runner.debugMode = true;
+            runner.Run();
+            Console.WriteLine("LOG=" + runner.PrintLog);
+            cout = "----------";
+
+            // Wait
+            cout = "ok.";
+            Console.ReadLine();
+        }
+
+        static void _w(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        static string cout
+        {
+            set { Console.WriteLine(value); }
+        }
+    }
+}
