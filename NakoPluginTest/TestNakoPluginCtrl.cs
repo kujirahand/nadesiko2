88162615bd8272c0651c595251f79b1b9ab54b02
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2011-05-01 15:39:25 +0000

    改めてプラグインのテストを追加。

---
+++ NakoPluginTest/TestNakoPluginCtrl.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * NUnit Test Case for NakoPluginCtrl
+ * User: kujirahand
+ * Date: 2010-10-04
+ * Time: 2:27
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Libnako.JPNCompiler;
+using Libnako.Interpreter;
+using Libnako.NakoAPI;
+
+using NakoPluginCtrl;
+
+namespace NakoPluginTest
+{
+    /// <summary>
+    /// Test for NakoPluginCtrl.
+    /// </summary>
+    [TestFixture]
+    public class TestNakoPluginCtrl
+    {
+        NakoCompiler com;
+        NakoInterpreter runner = new NakoInterpreter();
+        
+        public TestNakoPluginCtrl()
+        {
+            NakoCompilerLoaderInfo info = new NakoCompilerLoaderInfo();
+            info.PreloadModules = new NakoPlugin.INakoPlugin[] {
+                new NakoBaseSystem(),
+                new NakoPluginCtrl.NakoPluginCtrl()
+            };
+            com = new NakoCompiler(info);
+        }
+        
+        [Test][STAThreadAttribute]
+        public void TestClipboad()
+        {
+            com.DirectSource = 
+                "「abc」をコピー。\n" +
+                "クリップボードを表示。";
+            runner.Run(com.Codes);
+            Assert.AreEqual("abc", runner.PrintLog);
+        }
+        [Test][STAThreadAttribute]
+        public void TestClipboad2()
+        {
+            com.DirectSource = 
+                "10をコピー。\n" +
+                "クリップボードを表示。";
+            runner.Run(com.Codes);
+            Assert.AreEqual("10", runner.PrintLog);
+        }
+        [Test][STAThreadAttribute]
+        public void TestEnumWindows()
+        {
+            com.DirectSource = 
+                "窓列挙して表示。\n";
+            runner.Run(com.Codes);
+            Assert.AreNotEqual("", runner.PrintLog);
+        }
+    }
+}
---
+++ NakoPluginTest/TestNakoPluginHttp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Libnako.JPNCompiler;
+using Libnako.Interpreter;
+using Libnako.NakoAPI;
+
+using Libnako.JPNCompiler.ILWriter;
+
+using NakoPluginHttp;
+
+namespace NakoPluginTest
+{
+    [TestFixture]
+    public class TestNakoPluginHttp
+    {
+        NakoCompiler com;
+        NakoInterpreter runner = new NakoInterpreter();
+
+        public TestNakoPluginHttp()
+        {
+            NakoCompilerLoaderInfo info = new NakoCompilerLoaderInfo();
+            info.PreloadModules = new NakoPlugin.INakoPlugin[] {
+                new NakoBaseSystem(),
+                new NakoPluginHttp.NakoPluginHttp()
+            };
+            com = new NakoCompiler(info);
+        }
+
+        [Test]
+        public void TestUrlDecode()
+        {
+            com.DirectSource = 
+                "「%e3%81%aa%e3%81%a7%e3%81%97%e3%81%93」をURLデコードして表示。";
+            runner.Run(com.Codes);
+            Assert.AreEqual("なでしこ", runner.PrintLog );
+        }
+        [Test]
+        public void TestUrlEncode()
+        {
+            com.DirectSource = 
+                "「なでしこ」をURLエンコードして小文字変換して表示。";
+            runner.Run(com.Codes);
+            Assert.AreEqual("%e3%81%aa%e3%81%a7%e3%81%97%e3%81%93", runner.PrintLog );
+        }
+
+     }
+}
