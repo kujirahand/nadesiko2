62d6948ab11732ae7c116eb43dfa5d79a7863e4e
kujirahand <kujirahand@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2011-05-06 17:22:26 +0000

    試しにVB.NETでなでしこプラグイン作ってみた。PowerPointプラグイン（手抜き版）。

---
+++ trunk/NakoPluginOfficePowerPoint/NakoPluginOfficePowerPoint.vb
@@ -0,0 +1,126 @@
+﻿'
+' なでしこプラグイン (NakoPluginOfficePowerPoint)
+'
+' ---------------------------------------------------------------
+''' <summary>
+''' PowerPoint用のなでしこプラグイン(NakoPluginOfficePowerPoint)
+''' </summary>
+Public Class NakoPluginOfficePowerPoint
+    Implements NakoPlugin.INakoPlugin
+
+    ''' <summary>
+    ''' プラグインのフルネーム、これは変更不要
+    ''' </summary>
+    Public ReadOnly Property Name As String Implements NakoPlugin.INakoPlugin.Name
+        Get
+            Return Me.GetType().FullName
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' プラグインの説明
+    ''' </summary>
+    Public ReadOnly Property Description As String Implements NakoPlugin.INakoPlugin.Description
+        Get
+            Return "PowerPoint 操作プラグイン"
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' プラグインのバージョン
+    ''' </summary>
+    Public ReadOnly Property PluginVersion As Double Implements NakoPlugin.INakoPlugin.PluginVersion
+        Get
+            Return 1.0
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' ターゲットとするなでしこのバージョン
+    ''' </summary>
+    Public ReadOnly Property TargetNakoVersion As Double Implements NakoPlugin.INakoPlugin.TargetNakoVersion
+        Get
+            Return 2.0
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Usedの実装が必要
+    ''' </summary>
+    Private _userd As Boolean = False
+    Public Property Used As Boolean Implements NakoPlugin.INakoPlugin.Used
+        Get
+            Return _userd
+        End Get
+        Set(ByVal value As Boolean)
+            _userd = value
+        End Set
+    End Property
+
+    ' ---------------------------------------------------------------
+    ' DLL内で使う各種変数
+    ' ---------------------------------------------------------------
+    Dim oPPT As PowerPointLateWrapper
+
+    ''' <summary>
+    ''' なでしこメソッドの定義
+    ''' </summary>
+    ''' <param name="bank">定義用引数</param>
+    Public Sub DefineFunction(ByVal bank As NakoPlugin.INakoPluginBank) Implements NakoPlugin.INakoPlugin.DefineFunction
+        ' なでしこ関数の定義
+        bank.AddFunc("パワポ起動", "Vで", NakoPlugin.NakoVarType.Void, AddressOf _ppt_new, "可視状態V(オン/オフ)でPowerPointを起動する", "ぱわぽきどう")
+        bank.AddFunc("パワポ開く", "FILEを|FILEの", NakoPlugin.NakoVarType.Void, AddressOf _ppt_open, "PowerPointのファイルを開く", "ぱわぽひらく")
+        bank.AddFunc("パワポPNG出力", "DIRへ", NakoPlugin.NakoVarType.Void, AddressOf _ppt_saveAsPNG, "PowerPointをPNG形式でFILEという名前で出力する", "ぱわぽPNGしゅつりょく")
+        bank.AddFunc("パワポ終了", "", NakoPlugin.NakoVarType.Void, AddressOf _ppt_quit, "PowerPointを終了する", "ぱわぽしゅうりょう")
+    End Sub
+
+    Public Function _ppt_new(ByVal info As NakoPlugin.INakoFuncCallInfo) As Object
+        Dim i As Integer
+        i = info.StackPopAsInt()
+        oPPT = New PowerPointLateWrapper()
+        oPPT.SetVisible(i <> 0)
+        Return (Nothing)
+    End Function
+
+    Private Sub CheckPPT()
+        If (oPPT Is Nothing) Then
+            Throw New NakoPlugin.NakoPluginRuntimeException("先に『パワポ起動』でPowerPointを起動してください。")
+        End If
+    End Sub
+
+
+    Public Function _ppt_open(ByVal info As NakoPlugin.INakoFuncCallInfo) As Object
+        Dim f As String
+        f = info.StackPopAsString()
+        CheckPPT()
+        oPPT.Open(f)
+        Return (Nothing)
+    End Function
+
+    Public Function _ppt_saveAsPNG(ByVal info As NakoPlugin.INakoFuncCallInfo) As Object
+        Dim dir As String
+        dir = info.StackPopAsString()
+        CheckPPT()
+        oPPT.SaveToPngDir(dir)
+        Return (Nothing)
+    End Function
+
+    Public Function _ppt_quit(ByVal info As NakoPlugin.INakoFuncCallInfo) As Object
+        CheckPPT()
+        oPPT.Quit()
+        Return (Nothing)
+    End Function
+
+    Public Sub PluginFin(ByVal runner As NakoPlugin.INakoInterpreter) Implements NakoPlugin.INakoPlugin.PluginFin
+        If Not (oPPT Is Nothing) Then
+            oPPT.Quit()
+            oPPT.Dispose()
+            oPPT = Nothing
+        End If
+    End Sub
+
+    Public Sub PluginInit(ByVal runner As NakoPlugin.INakoInterpreter) Implements NakoPlugin.INakoPlugin.PluginInit
+        oPPT = Nothing
+    End Sub
+
+End Class
---
+++ trunk/NakoPluginOfficeTest/NakoPluginOfficePowerPointTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+using Libnako.JPNCompiler;
+using Libnako.Interpreter;
+using Libnako.NakoAPI;
+
+using NakoPluginOfficePowerPoint;
+
+namespace NakoPluginOfficeTest
+{
+    [TestFixture]
+    public class NakoPluginOfficePowerPointTest
+    {
+        NakoCompiler com;
+        NakoInterpreter runner;
+
+        public NakoPluginOfficePowerPointTest()
+        {
+            NakoCompilerLoaderInfo info = new NakoCompilerLoaderInfo();
+            info.PreloadModules = new NakoPlugin.INakoPlugin[] {
+                new NakoBaseSystem(),
+                new NakoPluginOfficePowerPoint.NakoPluginOfficePowerPoint()
+            };
+            com = new NakoCompiler(info);
+            runner = new NakoInterpreter();
+        }
+        [Test]
+        public void PowerPointTest()
+        {
+            com.DirectSource =
+                "0でパワポ起動。パワポ終了。\n";
+            runner.Run(com.Codes);
+            Assert.AreEqual(1,1); // dummy
+        }
+    }
+}
