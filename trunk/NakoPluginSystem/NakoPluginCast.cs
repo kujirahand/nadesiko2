4c0f9c57b760b522e2a590f6ba997dfc05016a88
shigepon0@gmail.com <shigepon0@gmail.com@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2013-09-09 14:15:25 +0000

    変数の型変換関数

---
+++ trunk/NakoPluginSystem/NakoPluginCast.cs
@@ -0,0 +1,41 @@
+using System;
+using NakoPlugin;
+
+namespace NakoPluginSystem
+{
+    public class NakoPluginCast : INakoPlugin
+    {
+        
+        string _description = "変数変換プラグイン";
+        Version _version = new Version(1, 0);
+        //--- プラグイン共通の部分 ---
+        public Version TargetNakoVersion { get { return new Version(2, 0); } }
+        public bool Used { get; set; }
+        public string Name { get { return this.GetType().FullName; } }
+        public Version PluginVersion { get { return _version; } }
+        public string Description { get { return _description; } }
+        //--- 関数の定義 ---
+        public void DefineFunction(INakoPluginBank bank)
+        {
+            bank.AddFunc("整数変換", "Sを", NakoVarType.Int, _to_int,"変数Sを整数に変換して返す", "せいすうへんかん");
+            bank.AddFunc("文字列変換", "Sを", NakoVarType.String, _to_string,"変数Sを文字列に変換して", "もじれつへんかん");
+        }
+        
+        // プラグインの初期化処理
+        public void PluginInit(INakoInterpreter runner)
+        {
+        }
+        // プラグインの終了処理
+        public void PluginFin(INakoInterpreter runner)
+        {
+        }
+       public object _to_int(INakoFuncCallInfo info){
+            return info.StackPopAsInt();
+        }
+        
+        public Object _to_string(INakoFuncCallInfo info){
+            return info.StackPopAsString();
+        }
+    }
+}
+
