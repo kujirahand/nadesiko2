4299e05937a304a23cf6c50bcf7f989739a424fd
shigepon0@gmail.com <shigepon0@gmail.com@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2013-09-17 06:29:25 +0000

    HTTP関連命令とテスト追加

--- NakoPluginTest/TestNakoPluginHttp.cs
+++ NakoPluginTest/TestNakoPluginHttp.cs
@@ -8,12 +8,12 @@ using Libnako.NakoAPI;
 
 using Libnako.JPNCompiler.ILWriter;
 
-using NakoPluginHttp;
+using NakoPlugin;
 
 namespace NakoPluginTest
 {
     [TestFixture]
-    public class TestNakoPluginHttp
+    public class TestNakoPluginHttp//TODO:簡易Webサーバを立ち上げる
     {
         NakoCompiler com;
         NakoInterpreter runner = new NakoInterpreter();
@@ -43,5 +43,62 @@ namespace NakoPluginTest
             Assert.AreEqual("%e3%81%aa%e3%81%a7%e3%81%97%e3%81%93", runner.PrintLog );
         }
 
+
+        [Test]
+        public void TestHttpGetData()
+        {
+            runner.Run(com.WriteIL(
+                "「http://www.nadesi.com/」をHTTPデータ取得して表示。"));
+            Assert.AreEqual(true,runner.PrintLog.Contains("なでしこ"));
+        }
+
+        [Test]
+        public void TestJsonDecode()
+        {
+            runner.Run(com.WriteIL(
+                "S=『{'href':'/account/login.aspx','target':'_blank'}』\n"+
+				"A=SをJSONデコード\n"+
+				"A[`target`]を表示。"));
+            Assert.AreEqual("_blank",runner.PrintLog);
+        }
+
+        [Test]
+        public void TestHttpHeader()
+        {
+            runner.Run(com.WriteIL(
+                "「http://nadesi.com/hoge」をHTTPヘッダ取得して表示。"));
+            Assert.AreEqual(true,runner.PrintLog.Contains("404 Not Found"));
+            runner.Run(com.WriteIL(
+                "「http://nadesi.com/」をHTTPヘッダ取得して表示。"));
+            Assert.AreEqual(true,runner.PrintLog.Contains("200 OK"));
+        }
+
+        [Test]
+        public void TestHttpHeaderHash()
+        {
+            runner.Run(com.WriteIL(
+                "A=「http://www.yahoo.co.jp/」をHTTPヘッダハッシュ取得。" +
+                "A[`Content-Type`]を表示。"));
+            Assert.AreEqual("text/html; charset=utf-8",runner.PrintLog);
+        }
+
+        [Test]
+        public void TestHttpPost()//TODO:外部サービスを使ってるので、後々簡易WEBサーバをテスト時に構築し、そこにアクセスするように変更したい
+        {
+            runner.Run(com.WriteIL(
+                "A=「http://www.muryou-tools.com/test/aaaa.php」へ「hoge=fuga&hage=1」をHTTPポスト。" +
+                "Aを表示。"));
+            Assert.AreEqual(true,runner.PrintLog.Contains("hoge=fuga"));
+        }
+
+        [Test]
+        public void TestHttpGet()//TODO:外部サービスを使ってるので、後々簡易WEBサーバをテスト時に構築し、そこにアクセスするように変更したい
+        {
+            runner.Run(com.WriteIL(
+                "A=「Referer:http://localhost/nadesiko2/\n"+
+                "UserAgent:Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0_1 like Mac OS X; ja-jp) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A306 Safari/6531.22.7」を「http://mpw.jp/requestheader/」へHTTPゲット。" +
+                "Aを表示。"));
+            Assert.AreEqual(true,runner.PrintLog.Contains("http://localhost/nadesiko2/"));
+        }
      }
 }
