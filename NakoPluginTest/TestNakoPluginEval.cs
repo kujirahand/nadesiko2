458d81682f0354ae2272c739a13160830f0fe11f
shigepon0@gmail.com <shigepon0@gmail.com@f8a7b404-0f05-3daa-6849-4e095ca8c858>
2014-05-08 00:33:50 +0000

    ナデシコする相当の機能を実装してみました。まだ修正する予定ですがとりあえず

---
+++ NakoPluginTest/TestNakoPluginEval.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Libnako.JPNCompiler;
+using Libnako.Interpreter;
+using Libnako.NakoAPI;
+
+using Libnako.JPNCompiler.ILWriter;
+
+using NakoPluginEval;
+
+namespace NakoPluginTest
+{
+	[TestFixture]
+	public class TestNakoPluginEval
+	{
+		NakoCompiler com;
+		NakoInterpreter runner = new NakoInterpreter();
+
+		public TestNakoPluginEval()
+		{
+			NakoCompilerLoaderInfo info = new NakoCompilerLoaderInfo();
+			info.PreloadModules = new NakoPlugin.INakoPlugin[] {
+				new NakoBaseSystem(),
+				new NakoPluginTag.NakoPluginTag()
+			};
+			com = new NakoCompiler(info);
+		}
+
+		[Test]
+		public void TestEval()
+		{
+			runner.Run(com.WriteIL( 
+				"S=「それ=1」をNadesiko\n" +
+				"Sを表示。"));
+			Assert.AreEqual("1",runner.PrintLog);
+		}
+	}
+}
